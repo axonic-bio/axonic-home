1d54c95311f6c54b8806e780fde1de2c4c94c22b
Anshul Kashyap <anshul@dhcp-10-29-255-154.dyn.mit.edu>
2024-10-24 14:23:44 -0400

    light mode

--- app/page.tsx
+++ app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
     <div onMouseMove={onMouseMove} className="content-center h-screen w-screen">
       <main className="content-center h-screen w-screen">
         <div className="h-screen w-screen">
-          <h1 className="absolute text-center content-center text-7xl z-10 w-screen h-screen --font-geist-mono text-white text-opacity-80">AXONIC</h1>
+          <h1 className="absolute text-center content-center text-7xl z-10 w-screen h-screen --font-geist-mono text-gray-800 text-opacity-50 dark:text-white dark:text-opacity-80">AXONIC</h1>
           <Line data={chartData} options={options} className="absolute z-0 h-screen pt-[10%]"/>
         </div>
       </main>
